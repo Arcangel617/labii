9cd1c605328f03ed2b2f5cc0ebd77f5348f92c7b
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-08-25 18:21:13 -0300

    Modifico el archivo base

--- Practicos/BatallaNaval/BatallaNaval.java
+++ Practicos/BatallaNaval/BatallaNaval.java
@@ -1,15 +1,15 @@
 public class BatallaNaval {
 	public static void main(String[] args) {
-		
+		imprimirTablero(5);
 	}
 
-	public static void imprimirTablero(){
+	public static void imprimirTablero(int tam){
 		for (int i = 0; i < tam; i++){
 			for (int j = 0; j < tam ; i++ ) {
 				if (j == (tam-1)) {
-					System.out.println("O")
+					System.out.println("O");
 				} else {
-					System.out.print("O ")
+					System.out.print("O ");
 				}
 			}
 		}
