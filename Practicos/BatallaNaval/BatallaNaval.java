55fac0b972263584b0cddde79dc6af839fc71360
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-08-25 18:18:23 -0300

    Agrego un gitignore y creo un archivo base

---
+++ Practicos/BatallaNaval/BatallaNaval.java
@@ -0,0 +1,17 @@
+public class BatallaNaval {
+	public static void main(String[] args) {
+		
+	}
+
+	public static void imprimirTablero(){
+		for (int i = 0; i < tam; i++){
+			for (int j = 0; j < tam ; i++ ) {
+				if (j == (tam-1)) {
+					System.out.println("O")
+				} else {
+					System.out.print("O ")
+				}
+			}
+		}
+	}
+}
